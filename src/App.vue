9a6fbf8912acbcc42d0339eca4cff823b2335100
K <592031129@qq.com>
2023-03-26 22:14:15 +0800

    🎈 perf: 完善

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <graph class="graph"></graph>
+  <graph grid snapline keyboard clipboard ref="gRef"></graph>
 </template>
 
 <script setup>
 import { Graph } from '@/lib/x6-data-driven'
+import { ref, onMounted } from 'vue'
+
+const gRef = ref()
+
+onMounted(() => {
+  const { graph } = gRef.value
+  graph.addNode({
+    width: 100,
+    height: 50,
+    label: 'hello'
+  })
+
+  graph.centerContent()
+})
 </script>
 
-<style lang="scss" scoped>
-.graph {
-  width: 100%;
-  height: 100%;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/lib/x6-data-driven/Graph.jsx
+++ src/lib/x6-data-driven/Graph.jsx
@@ -1,21 +1,52 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, shallowReactive, provide, onMounted, computed } from 'vue'
+import { Graph as X6Graph } from '@antv/x6'
+
+import styles from './graph.module.scss'
+import { getCurrentInstance } from '@vue/runtime-core'
+
+export const contextSymbol = String(Symbol('x6ContextSymbol'))
 
 export const Graph = defineComponent({
   name: 'x6Graph',
-  // inheritAttrs: false,
-  setup() {
+  inheritAttrs: false,
+  setup(props, { attrs, expose }) {
+    const { ...other } = attrs
     const container = ref()
+    const context = shallowReactive({ graph: null })
+    provide(contextSymbol, context)
+    expose(context)
+
+    onMounted(() => {
+      // options
+      Object.keys(other).forEach((key) => other[key] === '' && (other[key] = true))
+
+      console.log('1', container)
+
+      const size = computed(() => ({
+        width: container.value.clientWidth,
+        height: container.value.clientHeight
+      }))
+
+      console.log('2', {
+        container: container.value,
+        autoResize: true,
+        width: size.value.width,
+        height: size.value.height,
+        ...other
+      })
+      if (container.value)
+        context.graph = new X6Graph({
+          container: container.value,
+          autoResize: true,
+          width: size.value.width,
+          height: size.value.height,
+          ...other
+        })
+    })
 
     return () => {
       return (
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'relative'
-          }}
-        >
-          its x6 component
+        <div className={styles['x6-data-driven']}>
           <div ref={container}></div>
         </div>
       )
